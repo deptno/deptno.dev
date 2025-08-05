856666dbeab2196ddefa7ccf159e992bc55a887e
deptno <deptno@gmail.com>
2025-08-05 20:11:44 +0900

    feat: page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
+    <main className="min-h-dvh flex flex-col">
+      <div className="grow h-full flex flex-col justify-center items-center gap-2">
+        <div>deptno.</div>
+        <a className="underline" href="https://wiki.deptno.dev">wiki.</a>
+      </div>
     </main>
   );
 }
